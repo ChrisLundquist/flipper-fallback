721e2792fbc98f72aee4b7e11d7ed415127bd05f
Chris Lundquist <chris.lundquist@github.com>
2014-07-25 19:50:43 -0700

    cleanup default files to match flipper-redis

--- lib/flipper/adapters/fallback/version.rb
+++
@@ -1,7 +0,0 @@
-module Flipper
-  module Adapters
-    class Fallback
-      VERSION = "0.0.1"
-    end
-  end
-end
--- lib/flipper/fallback/version.rb
+++
@@ -1,5 +0,0 @@
-module Flipper
-  module Fallback
-    VERSION = "0.0.1"
-  end
-end
